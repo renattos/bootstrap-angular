5eeed3c6b722b42ae6fcae3351be1de7cb370eb8
Renato <renattos@gmail.com>
2017-01-16 00:58:43 -0200

    Correção das rotas

--- BootstrapAngular/WebContent/app/modulo1/modulo1.js
+++ BootstrapAngular/WebContent/app/modulo1/modulo1.js
@@ -16,13 +16,13 @@ angular.module('modulo1', [
       templateUrl: "app/modulo1/modulo1.html",
       controller: 'modulo1.Module1Ctrl'
     })
-    .state('modulo1.menu1', {
-    	url: "/menu1",
+    .state('modulo1/menu1', {
+    	url: "/modulo1/menu1",
     	templateUrl: "app/modulo1/menu1.html",
     	controller: 'modulo1.Menu1Ctrl'
     })
-    .state('modulo1.menu2', {
-    	url: "/menu2",
+    .state('modulo1/menu2', {
+    	url: "/modulo1/menu2",
     	templateUrl: "app/modulo1/menu2.html",
     	controller: 'modulo1.Menu2Ctrl'
     })
@@ -30,8 +30,8 @@ angular.module('modulo1', [
 })
 .controller('modulo1.Module1Ctrl', function($scope, $state, PlatformService) {
 	console.log('entrou no modulo 1');
-	PlatformService.sidebar.itens.push({name: 'Menu 1.1', state:'.menu1'});
-	PlatformService.sidebar.itens.push({name: 'Menu 1.2', state:'.menu2'});
+	PlatformService.sidebar.itens.push({name: 'Menu 1.1', state:'modulo1/menu1'});
+	PlatformService.sidebar.itens.push({name: 'Menu 1.2', state:'modulo1/menu2'});
 })
 .controller('modulo1.Menu1Ctrl', function($scope, $state) {
 	console.log('entrou no menu 1');
--- BootstrapAngular/WebContent/app/modulo2/modulo2.js
+++ BootstrapAngular/WebContent/app/modulo2/modulo2.js
@@ -17,13 +17,13 @@ angular.module('modulo2', [
       templateUrl: "app/modulo2/modulo2.html",
       controller: 'module2.Module2Ctrl'
     })
-    .state('modulo2.menu1', {
-    	url: "/menu1",
+    .state('modulo2/menu1', {
+    	url: "/modulo2/menu1",
     	templateUrl: "app/modulo2/menu1.html",
     	controller: 'module2.Menu1Ctrl'
     })
-    .state('modulo2.menu2', {
-    	url: "/menu2",
+    .state('modulo2/menu2', {
+    	url: "/modulo2/menu2",
     	templateUrl: "app/modulo2/menu2.html",
     	controller: 'module2.Menu2Ctrl'
     })
@@ -32,8 +32,8 @@ angular.module('modulo2', [
 
 .controller('module2.Module2Ctrl', function($scope, $state, PlatformService) {
 	console.log('entrou no modulo 2');
-	PlatformService.sidebar.itens.push({name: 'Menu 2.1', state:'modulo2.menu1'});
-	PlatformService.sidebar.itens.push({name: 'Menu 2.2', state:'modulo2.menu2'});
+	PlatformService.sidebar.itens.push({name: 'Menu 2.1', state:'modulo2/menu1'});
+	PlatformService.sidebar.itens.push({name: 'Menu 2.2', state:'modulo2/menu2'});
 })
 .controller('module2.Menu1Ctrl', function($scope, $state) {
 	console.log('entrou no menu 1');
